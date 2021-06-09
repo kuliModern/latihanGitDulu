25b3d05d9c00c148c2652ed411f1ed42aaa8072b
zkake <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-06-09 17:26:11 +0700

    access dan control

---
+++ latihanGitDulu/Access/access.swift
@@ -0,0 +1,16 @@
+//
+//  access.swift
+//  latihanGitDulu
+//
+//  Created by Azka Kusuma on 09/06/21.
+//
+
+import Foundation
+
+enum Access {
+    case electrical
+    case fuel
+    case engine
+    case cockpit
+    case control
+}
